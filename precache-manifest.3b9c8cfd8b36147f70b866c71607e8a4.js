60482903aee3caf36b5c7c467bb6d6a3ebc0d614
iamShashi <Shashi.chandra101@gmail.com>
2020-07-24 23:49:09 +0530

    Updates

---
+++ precache-manifest.3b9c8cfd8b36147f70b866c71607e8a4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4077d989477481272108ff6ca3c6da2",
+    "url": "/movielist-using-react/index.html"
+  },
+  {
+    "revision": "433a9f469dca9eb08c3f",
+    "url": "/movielist-using-react/static/css/2.ffa14ab8.chunk.css"
+  },
+  {
+    "revision": "38c37dd2a214d1652753",
+    "url": "/movielist-using-react/static/css/main.274422bd.chunk.css"
+  },
+  {
+    "revision": "433a9f469dca9eb08c3f",
+    "url": "/movielist-using-react/static/js/2.75d7e760.chunk.js"
+  },
+  {
+    "revision": "38c37dd2a214d1652753",
+    "url": "/movielist-using-react/static/js/main.0a7fdb31.chunk.js"
+  },
+  {
+    "revision": "711a5ec9f3a11843d749",
+    "url": "/movielist-using-react/static/js/runtime~main.5ad5b318.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movielist-using-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movielist-using-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movielist-using-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movielist-using-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movielist-using-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/movielist-using-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
